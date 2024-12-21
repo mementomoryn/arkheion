ee9464a06c6ac1fc7a7f70bc40fa0320eb116abc
mementomoryn <132916305+mementomoryn@users.noreply.github.com>
2024-12-21 23:48:27 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -20,3 +20,7 @@ def download(link, out, headers=None):
                 
 def run_command(command: list[str]):
     os.system(" ".join(command))
+
+def set_github_output(name, value):
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        print(f'{name}={value}', file=fh)
