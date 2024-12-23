ab1d1121a7619e58b747fa8cce5caa2db6ef2391
mementomoryn <132916305+mementomoryn@users.noreply.github.com>
2024-12-23 23:19:30 +0800

    Update spotdl_module.py

--- spotdl_module.py
+++ spotdl_module.py
@@ -1,26 +1,52 @@
 import os
-import json
 from utils import panic, run_command
 
 query = os.environ["QUERY"]
 codec = os.environ["CODEC"]
+client_id = os.environ["CLIENT_ID"]
+client_secret = os.environ["CLIENT_SECRET"]
 
-command = [
-    'spotdl'
+output_path = "music/{artist}/{album} - {year}/{artist} - {title}.{output-ext}"
+
+audio_providers = [
+    "youtube_music",
+    "bandcamp",
+    "soundcloud",
+    "youtube"
 ]
 
-with open("configs/spotdl.json") as config:
-    for key, values in json.load(config).items():
-        if key != 'format' or codec == 'ORIGINAL':
-            command.append(f"--{key}")
-            if type(values) is str:
-                command.append(values)
-            else:
-                command.append(str(values).lower())
+lyrics_providers = [
+    "musixmatch",
+    "synced",
+    "genius",
+    "azlyrics"
+]
 
+command = [
+    'spotdl',
+    '--client-id',
+    client_id,
+    '--client-secret',
+    client_secret,
+    '--audio',
+    audio_providers,
+    '--lyrics',
+    lyrics_provider,
+    '--sponsor-block',
+    '--no-cache',
+    '--preload',
+    '--dont-filter-results',
+    '--generate-lrc',
+    '--print-errors
+    '--output',
+    output_path
+]
+
+command.append('--format')
 if codec != 'ORIGINAL':
-    command.append('--format')
     command.append(codec.lower())
+else:
+    command.append('opus')
 
 command.append('download')
 
@@ -28,7 +54,6 @@ command.append(query.strip())
 
 print(f"[INFO] {query}, codec: {codec}")
 
-print(command)
 run_command(command)
 
 # if os.path.exists("music") is False:
