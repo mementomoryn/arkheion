9dd32218a5f5a87ec5062aada3d240b48757a02c
mementomoryn <132916305+mementomoryn@users.noreply.github.com>
2024-12-26 20:42:46 +0800

    Update run.py

--- modules/tidal-dl-ng/run.py
+++ modules/tidal-dl-ng/run.py
@@ -7,14 +7,14 @@ query = os.environ["QUERY"]
 quality = os.environ["QUALITY"]
 
 config_file = "'modules/tidal-dl-ng/config.json'"
-print("Symlinking configured config.json...")
+print("Moving configured config.json...")
 if os.path.isfile(config_file) == True:
-  run_command(["ln", "-f", config_file, "'$HOME/.config/tidal_dl_ng/settings.json'"])
+  run_command(["mv", "-f", config_file, "'$HOME/.config/tidal_dl_ng/settings.json'"])
 
 token_file = "'modules/tidal-dl-ng/token.json'"
-print("Symlinking user configured token.json...")
+print("Moving user configured token.json...")
 if os.path.isfile(token_file) == True:
-  run_command(["ln", "-s", token_file, "'$HOME/.config/tidal_dl_ng/token.json'"])
+  run_command(["mv", "-f", token_file, "'$HOME/.config/tidal_dl_ng/token.json'"])
 
 if quality != "AUTO":
     run_command(["tidal-dl-ng", "cfg", "quality_audio", quality.replace(" ", "_")])
@@ -37,7 +37,7 @@ command.append(cli_commands(select))
 if select == "DOWNLOAD":
   command.append(query.strip())
 
-subprocess.run(command).check_returncode()
+run_command(command)
 
 # if os.path.exists("music") is False:
     # panic("[ERROR] Downloads folder does not exist")
