dc9b2e29efa2a7e0212c965dd3225d3f8624c295
mementomoryn <132916305+mementomoryn@users.noreply.github.com>
2024-12-26 14:42:00 +0800

    Update run.py

--- modules/spotdl/run.py
+++ modules/spotdl/run.py
@@ -2,7 +2,7 @@ import os
 from utils import panic, run_command
 
 query = os.environ["QUERY"]
-query_untracked = query.split("?")[0]
+query_untracked = query.split("?si=")[0]
 codec = os.environ["CODEC"]
 client_id = os.environ["CLIENT_ID"]
 client_secret = os.environ["CLIENT_SECRET"]
@@ -57,8 +57,6 @@ command.append(query_untracked.strip())
 
 command.append('--output')
 command.append(output_path)
-
-print(f"[INFO] {query_untracked.strip()}, codec: {codec}")
 
 run_command(command)
 
