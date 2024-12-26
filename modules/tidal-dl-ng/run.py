1fd765d49f8962f2176f5f1d9bfd7f3f920d3aee
mementomoryn <132916305+mementomoryn@users.noreply.github.com>
2024-12-26 14:38:53 +0800

    Create run.py

---
+++ modules/tidal-dl-ng/run.py
@@ -0,0 +1,26 @@
+import os
+from utils import panic, run_command
+
+select = os.environ["COMMANDS"]
+query = os.environ["QUERY"]
+
+def cli_commands(select):
+    match (select):
+        case "DOWNLOAD":
+            return "dl"
+        case "LOGIN":
+            return "login"
+
+command = [
+    'tdn'
+]
+
+command.append(map(cli_commands, select)
+
+if select == "DOWNLOAD"
+  command.append(query.strip())
+
+run_command(command)
+
+# if os.path.exists("music") is False:
+    # panic("[ERROR] Downloads folder does not exist")
